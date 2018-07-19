f61dfebd52c68f95a95077de37a4475414736b77
Maaz Irfan <muhammadmaazirfan@outlook.com>
2018-07-19 19:12:34 +0500

    updated project

---
+++ DesktopServerLeapMotion-master/Assets/thumbInteraction.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+public class thumbInteraction : MonoBehaviour {
+
+    public AudioClip Clips;
+    public AudioSource Cli;
+
+	// Use this for initialization
+	void Start () {
+       
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Cli.Play();
+
+        AudioSource audio = GetComponent<AudioSource>();
+        audio.clip = Clips;
+
+        audio.Play();
+    }
+}
---
+++ VR LeapMotion/Assets/fidgetBehavoiur.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class fidgetBehavoiur : MonoBehaviour {
+
+    private Vector3 originalPosition;
+    float XAngle;
+    private Rigidbody rb;
+	// Use this for initialization
+	void Start () {
+
+        originalPosition = transform.localPosition;
+        XAngle = transform.eulerAngles.x;
+        rb = GetComponent<Rigidbody>();
+
+    //    rb.AddForce(transform.position);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        transform.localPosition = originalPosition;
+        transform.eulerAngles = new Vector3(XAngle, 0, transform.eulerAngles.z);
+	}
+
+    private void OnCollisionExit(Collision collision)
+    {
+        rb.AddForce(collision.transform.position);
+    }
+
+}
